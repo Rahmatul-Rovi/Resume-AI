6b4b2394610e6f76599e53d9daf79b9aeff4a2ad
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-06-08 00:03:17 +0600

    Analyze Section Updated

--- app/api/analyze/route.ts
+++ app/api/analyze/route.ts
@@ -3,5 +3,18 @@ import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 import { GoogleGenerativeAI } from '@google/generative-ai'
+import { error } from 'console'
  
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
+
+export async function POST(req:Request) {
+    const session = await getServerSession(authOptions)
+    if(!session?.user?.id){
+        return NextResponse.json({error: 'UnAuthorized'}, {status: 401})
+    }
+    try{
+
+    } catch(error){
+        
+    }
+}
